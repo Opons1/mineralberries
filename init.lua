71a26c2cde305f53111beeb0b4a75e4a8ec157ff
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-02-17 12:39:48 -0800

    more ideas

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 --three types of bush for a total of 6 nodes per mineral. 
 --small, large, and overgrown(TBD)
 --they will require an ore below them to grow, with overgrown consumng the ore after making 10 berries(TBD)
+--small can not require an ore, but it dies after a while.
 --easy api to register them hopefuly
 --mberries_register(ore, ore_node, berries_per_ore, rarity)
 --by default bushes will spawn on the ore, with 3 adjacent blocks being air (via core.register_decoration)
