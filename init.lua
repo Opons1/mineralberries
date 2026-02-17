9ae32a4058356b86e4ef73b2a30d9c08f703f1c9
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-02-17 12:29:14 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
-
+--GOALS
+--three types of bush for a total of 6 nodes per mineral. 
+--small, large, and overgrown(TBD)
+--they will require an ore below them to grow, with overgrown consumng the ore after making 10 berries(TBD)
+--easy api to register them hopefuly
+--mberries_register(ore, ore_node, berries_per_ore, rarity)
+--by default bushes will spawn on the ore, with 3 adjacent blocks being air (via core.register_decoration)
+--faint glow(luminance: 3 maybe?)
+--can die when no ore below
+--a book detailing some info about oreberries spawning in dungeons maybe?
+--oreberries can be crafted into the ore block
+--technic support to maybe have an extractor make it go ot dust?
+--support for multiple mods
+function oreberries_register_bush(ore_node, ore, rarity, berries_per_ore, berries_per_bush, variants)
+  
+end
